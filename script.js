e586748f712d01f3fae76c5df3df766d179f00ef
Mark Anthony Patrick Rodriguez <markrodriguez003@gmail.com>
2020-05-25 22:37:22 -0400

    Added some functionality and visuals

--- script.js
+++ script.js
@@ -9,15 +9,43 @@ $(document).ready(() => {
    const API_CURRENT_URL = "https://api.openweathermap.org/data/2.5/weather?q="  /* https://openweathermap.org/current */
    const API_FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast?q=" /* https://openweathermap.org/forecast5 */
    let SEARCH_CITY;
+   let userHistory = [];
+   localStorage.setItem("LS_userHistory", "");
+ /* grab user's location with window? then call APICall() to display temp data automatically when user visits page */
+  
+ /* Loads user History */
+ loadHistory = () => {
+   $(".left-search-panel-search-history").empty();
+   
+/*    let parsed_LS_History = JSON.parse(localStorage["LS_userHistory"]); */
+   
+   /* if(localStorage.getItem("LS_userHistory")> 0){
 
-   /* grab user's location with window? then call APICall() to display temp data automatically when user visits page */
+      let parsed_LS_History = JSON.parse(localStorage["LS_userHistory"]);
+    
+      for(let i = 0; i < parsed_LS_History.length; i++){
+         setTimeout(() => {
+            $(".left-search-panel-search-history ul").append(
+               ` <li class="grid-x small-2 medium-12 large-12 slide-right" style="text-overflow: ellipsis !important">${parsed_LS_History[i]}</li>`
+            )
+         }, 200)
+   
+      }
+    
+   } */
 
- 
+ }
 
+
+
+
+
+   /* RANDOM CITY GENERATOR  */
    city_pageLoader = () => {
       $(".left-search-panel-cities").empty();
       /* LOADS RANDOM CITIES */
-      for (let i = 0; i < 12; i++) {
+      loadHistory();
+      for (let i = 0; i < 6; i++) {
          const cities = [
             "Miami",
             "Orlando",
@@ -83,7 +111,7 @@ $(document).ready(() => {
             "Philadelphia",
             "Atlantic City",
             "Vineland",
-            "Cherry Hilly",
+            "Cherry Hill",
             "Scranton",
             "Hartford",
             "Springfield",
@@ -132,31 +160,39 @@ $(document).ready(() => {
    }//eof city_pageLoader
 
    /* GRABS RANDOM CITY FROM LEFT PANEL AND LOADS IT IN SEARCH FIELD */
-   $(document).on("click", $(".left-search-panel-cities li"), (e) => {
-      console.log(e.target.innerText);
-      $("#citySearch").val(e.target.innerText);
+   $(document).on("click",".left-search-panel-cities li", (e) => {
+      e.stopPropagation();
+
+      $("#citySearch").val(e.target.innerText.toString().toLowerCase());
+     
    })
 
    city_pageLoader();
 
    /* USER PRESSES SEARCH BUTTON */
-   $("#searchBtn").on("click", () => {
-      SEARCH_CITY = $("#citySearch").val();
+   $("#searchBtn").on("click", ( ) => {
+      SEARCH_CITY = $("#citySearch").val().toString().toLowerCase();
       $("#citySearch").css("background-color", "white");
-      console.log(`Search City: ${SEARCH_CITY}`);
+
+      if(userHistory.indexOf(SEARCH_CITY) === -1){
+         userHistory.push(SEARCH_CITY);
+  
+         localStorage.setItem("LS_userHistory", JSON.stringify(userHistory));
+         
+      }  
+      
+     
 
       API_call(SEARCH_CITY);
-      
+
    })
    /* USER PRESSES ENTER */
    $(document).on('keypress', function (e) {
       if (e.which === 13) {
-         console.log("You Pressed Enter!");
          SEARCH_CITY = $("#citySearch").val();
          $("#citySearch").css("background-color", "white");
-         console.log(`Search City: ${SEARCH_CITY}`);
          API_call(SEARCH_CITY);
-        
+
       }
    });
 
@@ -167,22 +203,22 @@ $(document).ready(() => {
          url: SEARCH_URL,
          method: "GET",
          success: (response) => {
-            console.log(response);
+      /*       console.log(response); */
             $(".map").empty();
             $("#current_description").text(response.weather[0].description);
             $("#weatherIcon").attr("src", `http://openweathermap.org/img/w/${response.weather[0].icon}.png`);
             $("#weatherIcon").css("display", `block-inline`);
 
-            if(response.weather[0].icon[2] === "d"){
-               $(".right-detail-panel").css({"background-image": "url(assets/dayTimeBG.png)", "background-repeat": "no-repeat", "background-size": "cover"});
-   /*             $(".right-detail-panel").class("slide-fwd-center"); */
-               
-            } else if(response.weather[0].icon[2] === "n"){
-               $(".right-detail-panel").css({"background-image": "url(assets/nightTimeBG.png)", "background-repeat": "no-repeat", "background-size": "cover"});
-               $(".right-detail-panel").css("color", "white");
-            /*    $(".right-detail-panel").class("slide-fwd-center"); */
+            if (response.weather[0].icon[2] === "d") {
+               $(".right-detail-panel").css({ "background-image": "url(assets/dayTimeBG.png)", "background-repeat": "no-repeat", "background-size": "cover" });
+               /*             $(".right-detail-panel").class("slide-fwd-center"); */
 
-               
+            } else if (response.weather[0].icon[2] === "n") {
+               $(".right-detail-panel").css({ "background-image": "url(assets/nightTimeBG.png)", "background-repeat": "no-repeat", "background-size": "cover" });
+               $(".right-detail-panel").css("color", "white");
+               /*    $(".right-detail-panel").class("slide-fwd-center"); */
+
+
             } else {
                $(".right-detail-panel").css("background-color", "tomato");
 
@@ -196,15 +232,15 @@ $(document).ready(() => {
             var map = new ol.Map({
                target: 'map',
                layers: [
-                 new ol.layer.Tile({
-                   source: new ol.source.OSM()
-                 })
+                  new ol.layer.Tile({
+                     source: new ol.source.OSM()
+                  })
                ],
                view: new ol.View({
-                 center: ol.proj.fromLonLat([`${response.coord.lon}`, `${response.coord.lat}`]),
-                 zoom: 10
+                  center: ol.proj.fromLonLat([`${response.coord.lon}`, `${response.coord.lat}`]),
+                  zoom: 10
                })
-             });
+            });
 
             const UVI_URL = `https://api.openweathermap.org/data/2.5/uvi?${API_KEY}&lat=${response.coord.lat}&lon=${response.coord.lon}`;
             $.ajax({
@@ -213,6 +249,65 @@ $(document).ready(() => {
             }).then((uvi) => {
                $("#current_UVI").text(uvi.value);
             });
+
+            const SEARCH_FUTURE_URL = API_FORECAST_URL + SEARCH_CITY + API_KEY;
+            $.ajax({
+               url: SEARCH_FUTURE_URL,
+               method: "GET",
+               success: (future_response) => {
+
+
+                  let futureWeather_ObjArr = [];
+                  let futureWeather_Obj = {
+                     date:"",
+                     icon:"",
+                     descrip: "",
+                     temp: "",
+                     humidity: ""
+                  };
+
+            /*       console.log(future_response); */
+
+                  for(let i = 0; i < 5; i++){
+                     futureWeather_Obj.date = future_response.list[i].dt_txt;
+                     futureWeather_Obj.icon = future_response.list[i].weather[0].icon;
+                     futureWeather_Obj.descrip = future_response.list[i].weather[0].description;
+                     futureWeather_Obj.temp = future_response.list[i].main.temp;
+                     futureWeather_Obj.humidity = future_response.list[i].main.humidity;
+                     futureWeather_ObjArr.push(futureWeather_Obj);
+                  }
+
+                /*   console.log(futureWeather_ObjArr); */
+
+                  /* console.log(future_response);
+                  console.log(future_response.list[0].dt_txt);
+                  console.log(future_response.list[0].weather[0].description); 
+                  console.log(future_response.list[0].weather[0].icon); 
+                  console.log(future_response.list[0].main.temp); 
+                  console.log(future_response.list[0].main.humidity); */
+
+
+
+                  /*   console.log($(`.card-section ul li span`)[0].innerText = `${future_response.list[`${0}`].dt_txt}`);
+                    console.log($(`.card-section ul li span`)[1].innerText = `${future_response.list[`${0}`].weather[0].description}`);
+                    console.log($(`.card-section img`)[0]);
+                    console.log($(`.card-section ul li span`)[3]);
+                    console.log($(`.card-section ul li span`)[4]);
+                 */
+                  /* for (let i = 0; i <= 15; i++) {
+                       console.log($(`.card-section ul li`)[i]); 
+                      $(`.card-section ul li`)[i].text(future_response.list[`${i}`].dt_txt);
+                        $(`.card-section ul li`)[i].text(future_response.list[`${i}`].weather[0].description);
+                        $(`.card-section ul li`)[i + 1].attr("src", `http://openweathermap.org/img/w/${future_response.list[`${i}`].weather[0].icon}.png`);
+                        $(`.card-section ul li`)[i + 2].text(getFahrenheit(future_response.list[`${i}`].main.temp));
+                        $(`.card-section ul li`)[i + 3].text(future_response.list[`${i}`].main.humidity);
+                        i+=4; 
+                  }*/
+
+               }
+            });
+
+
             city_pageLoader();
          },
          error: () => {
@@ -224,8 +319,6 @@ $(document).ready(() => {
 
          }
       })
-
-
 
    } // api call function end
 
